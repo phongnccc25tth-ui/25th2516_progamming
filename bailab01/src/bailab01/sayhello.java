1c79d5074cce2bf4670d022d6d41f7b48ab295d6
phongnccc25tth-ui <phong.nc.cc25tth@ntu.edu.vn>
2026-08-17 21:32:08 +0700

    Create sayhello.java

---
+++ bailab01/src/bailab01/sayhello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package bailab01;
+
+/**
+ * 
+ */
+public class sayhello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("xin chao ");
+	}
+
+}
